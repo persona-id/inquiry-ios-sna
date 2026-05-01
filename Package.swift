7c31c2e34b558349e53fbf7d807bb7596f7ca757
mobile-build-persona <mobile-build@withpersona.com>
2026-04-30 22:51:25 -0600

    Update Package.swift for 2.46.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "PersonaSna",
-      url: "https://github.com/persona-id/inquiry-ios-sna/releases/download/2.46.0-RC/PersonaSna.xcframework.zip",
+      url: "https://github.com/persona-id/inquiry-ios-sna/releases/download/2.46.0/PersonaSna.xcframework.zip",
       checksum: "44c34fc5e3f21feb6980a87f7eaa8957ba3b7535f5767efc791c83a5d89c6f3b"
     )
   ]
