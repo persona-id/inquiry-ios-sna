9d6a76de1d2e992f4aa545660176540c9a3c942f
mobile-build-persona <mobile-build@withpersona.com>
2026-07-20 21:42:00 -0400

    Bump to 2.51.1-beta.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "PersonaSna",
-  platforms: [.iOS("15.0")],
+  platforms: [.iOS(.v15)],
   products: [
     .library(
       name: "PersonaSna",
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "PersonaSna",
-      url: "https://github.com/persona-id/inquiry-ios-sna/releases/download/3.5.0/PersonaSna.xcframework.zip",
-      checksum: "2b6ffbc3eeb2a2b0efa8e5845f5067b0ae08948213b298dbc4e12a555d8842bb"
+      url: "https://storage.googleapis.com/sdk.withpersona.com/ios/v2/releases/2.51.1-beta.3/PersonaSna.xcframework.zip",
+      checksum: "fa96e0af4296eb5bde02689818445560162ad17689ba913f01a6f4f057ceb216"
     )
   ]
 )
