4fcad22c78e1f5763ae4390ba89c7a2a840f278e
mobile-build-persona <mobile-build@withpersona.com>
2026-07-29 12:18:06 -0400

    Update Package.swift for 3.5.1-RC

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "PersonaSna",
-  platforms: [.iOS(.v15)],
+  platforms: [.iOS("15.0")],
   products: [
     .library(
       name: "PersonaSna",
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "PersonaSna",
-      url: "https://github.com/persona-id/inquiry-ios-sna/releases/download/2.52.1/PersonaSna.xcframework.zip",
-      checksum: "8c6b8718b4961a819ecaeaa4846794ee68219e41616601e8ed2f2a0d431a6a94"
+      url: "https://github.com/persona-id/inquiry-ios-sna/releases/download/3.5.1-RC/PersonaSna.xcframework.zip",
+      checksum: "0389552986f9f9fd98aa092c51de8a5a94c1f20d39b43730cd2a67cb784fb5cf"
     )
   ]
 )
